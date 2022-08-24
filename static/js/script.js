b49eab986b36cb8296dafb4f205e88283ebc874d
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-08-24 00:56:42 +0000

    Create open and close menu functions to open/close the more menu popup
    functions change the display and visibility of the menu and its
    background container. Also add/remove focus to the menu and alter
    the relevant aria attributes.

---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+// JS Section: DOM element constants:
+
+const moreMenuContainer = document.getElementById('more_menu_container');
+const moreMenu = document.getElementById('more_menu');
+const moreMenuButtons = document.getElementsByClassName('more_menu_button');
+
+// JS Section: Functions:
+
+function openMenu() {
+    moreMenuContainer.style.display = 'block';
+    moreMenu.style.visibility = 'visible';
+    moreMenu.focus();
+    moreMenuButtons[0].setAttribute('aria-expanded', 'True');
+}
+
+function closeMenu() {
+    moreMenu.style.removeProperty('visibility');
+    moreMenuButtons[0].setAttribute('aria-expanded', 'False');
+    moreMenuContainer.style.removeProperty('display');
+    moreMenu.blur();   
+}
