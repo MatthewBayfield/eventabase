70bf9d36cdedadc0520a9096766d79aa7a4a34d1
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-09-26 12:00:02 +0000

    Add class docstring to CustomUserModel

--- landing_page/models.py
+++ landing_page/models.py
@@ -11,9 +11,28 @@ from django.utils import timezone
 
 class CustomUserModel(AbstractBaseUser, PermissionsMixin):
     """
+    Custom user model for user account creation and user authentication.
+
+    Inherits password and last_login model fields from AbstractBaseUser.
+
+    Attributes:
+        username (character field)
+        email (character field)
+        is_staff (boolean field): indicates whether the user is a
+                                  site administrator.
+        is_superuser (boolean field): indicates whether the user has
+                                       all permissions.
+        is_active (boolean field): indicates if the account is active.
+        date_joined (Date-time field): specifies when the user signed-up.
+        USERNAME_FIELD (str): specifies name of field that acts as username.
+        REQUIRED_FIELDS (str): specifies required fields other than password and USERNAME-FIELD.
+        objects (obj): model user manager instance.
+
+
     """
     username = models.CharField(max_length=150, unique=True,
-                                help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+                                help_text="Required. 150 characters or fewer."
+                                          "Letters, digits and @/./+/-/_ only.",
                                 validators=[UnicodeUsernameValidator(),
                                             MaxLengthValidator(150)])
 
@@ -21,7 +40,8 @@ class CustomUserModel(AbstractBaseUser, PermissionsMixin):
                               validators=[EmailValidator()])
 
     is_staff = models.BooleanField(default=False, help_text="Designates whether the user can log into this admin site.",)
-    is_superuser = models.BooleanField(default=False, help_text="Designates whether this user should be treated as active.")
+    is_superuser = models.BooleanField(default=False, help_text="Designates that this user has all permissions without "
+                                                                "explicitly assigning them.")
     is_active = models.BooleanField("active", default=True,
                                     help_text="Designates whether this user should be treated as active."
                                               "Unselect this instead of deleting accounts.")
