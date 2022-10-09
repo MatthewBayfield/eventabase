28ca5282a74afd126fc34586ecce6f494dff2994
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-10-09 18:56:30 +0000

    Modify context variables in landing_page urls. Modify landing_page view
    New context variables added as a result of template changes. View
    modified to redirect logged in users to their home page when
    visiting the landing_page url.

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'landing_page'
 urlpatterns = [
-    path('', views.LandingPage.as_view(extra_context={'slogan': 'Events & Activities Organised by You for You', 'page_id': 'landing_page'}), name='landing_page_url')
+    path('', views.LandingPage.as_view(extra_context={'slogan': 'Events & Activities Organised by You for You',
+                                                      'page_id': 'landing_page',
+                                                      'logged_in': False}), name='landing_page_url')
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic.base import TemplateView
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -10,6 +11,17 @@ class LandingPage(TemplateView):
     """
     template_name = "landing_page/landing_page.html"
 
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET request
+        """
+        if request.user.is_authenticated:
+            return redirect(reverse('home:user_homepage'))
+        return super().get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
+        """
+        Returns template context
+        """
         context = super().get_context_data(**kwargs)
         return context
