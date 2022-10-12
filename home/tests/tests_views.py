0a6c827b421883266b23a9fbcbcf0d56989ee162
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-10-12 20:23:54 +0000

    Enable view to render a user's profile info in home page template. Test
    View now retrieves a user's profile information from models, and
    processes it into the context used to render the profile template
    as part of the rendered home page template. Tests written to check
    functionality.

--- home/tests/tests_views.py
+++ home/tests/tests_views.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.test import Client
 from allauth.account.models import EmailAddress
+from landing_page.models import CustomUserModel
+from home.models import UserAddress, UserProfile
+import datetime
 
 # Create your tests here.
 
@@ -20,10 +23,26 @@ class TestHomeViews(TestCase):
         client = Client()
         client.post('/accounts/signup/', self.data)
         # verify user email
-        user = EmailAddress.objects.get(user='tommypaul147@gmail.com')
-        user.verified = True
-        user.save()
-        
+        user_email = EmailAddress.objects.get(user='tommypaul147@gmail.com')
+        user_email.verified = True
+        user_email.save()
+        # create profile for user
+        user = CustomUserModel.objects.get(email=user_email.email)
+        UserProfile.objects.create(username=user,
+                                   first_name='jimmy',
+                                   last_name='knighton',
+                                   date_of_birth='1926-03-25',
+                                   sex='male',
+                                   bio='I enjoy all outdoor activities.')
+        # create address for user
+        UserAddress.objects.create(username=user.profile,
+                                   address_line_one='57 portland gardens',
+                                   city_or_town='chadwell heath',
+                                   county='essex',
+                                   postcode='rm65uh',
+                                   latitude=51.5791,
+                                   longitude=0.1355)
+
     def test_home_page_response_anonymous_user(self):
         """
         Tests status code for home page get request by an anonymous user.
@@ -46,6 +65,38 @@ class TestHomeViews(TestCase):
 
     def test_home_page_response_authenticated_user(self):
         """
+        Tests status code for home page get request by an authenticated user.
+        """
+        client = Client()
+        # sign-in user
+        response = client.login(email=self.data['email'],
+                                password=self.data['password1'])
+        self.assertTrue(response)
+        response = client.get('/home/')
+        response_code = response.status_code
+        self.assertEqual(response_code, 200)
+
+    def test_home_page_response_rendered_template_authenticated_user(self):
+        """
+        Tests the correct template is rendered for a home page url
+        get request by an authenticated user.
+        """
+        client = Client()
+        # sign-in user
+        response = client.login(email=self.data['email'],
+                                password=self.data['password1'])
+        self.assertTrue(response)
+        response = client.get('/home/')
+        self.assertTemplateUsed(response, 'home/home.html')
+        sub_context = {'page_id': 'home_page',
+                       'logged_in': True,
+                       'username': self.data['username']}
+        for key, value in sub_context.items():
+            with self.subTest(key):
+                self.assertEqual(response.context[key], value)
+
+    def test_landing_page_response_authenticated_user(self):
+        """
         Tests status code for landing page get request by an authenticated user.
         """
         client = Client()
@@ -60,7 +111,7 @@ class TestHomeViews(TestCase):
         self.assertEqual(response_code, 200)
         self.assertEqual(redirect_chain, [('/home/', 302)])
 
-    def test_home_page_response_rendered_template_authenticated_user(self):
+    def test_landing_page_response_rendered_template_authenticated_user(self):
         """
         Tests the correct template is rendered for a landing page url
         get request by an authenticated user.
@@ -84,3 +135,27 @@ class TestHomeViews(TestCase):
     # def test_authenticated_staff_user_home_page_response(self):
     #     """
     #     """
+
+    def test_profile_template_rendering(self):
+        """
+        """
+        client = Client()
+        # sign-in user
+        client.login(email=self.data['email'],
+                     password=self.data['password1'])
+        response = client.get('/home/')
+        self.assertTemplateUsed(response, 'home/profile.html')
+        expected_user_profile_data = {'first name': 'jimmy',
+                                      'last name': 'knighton',
+                                      'date of birth': datetime.date(1926, 3, 25),
+                                      'sex': 'male', 'bio': 'I enjoy all outdoor activities.'}.items()
+        expected_user_address_data = {'Address line 1': '57 portland gardens',
+                                      'City/Town': 'chadwell heath',
+                                      'County': 'essex',
+                                      'Postcode': 'rm65uh'}.items()
+
+        sub_context = {'user_profile_data': expected_user_profile_data,
+                       'user_address_data': expected_user_address_data}
+        for key, value in sub_context.items():
+            with self.subTest(key):
+                self.assertEqual(response.context[key], value)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
+from django.core.exceptions import ObjectDoesNotExist
+from django.template.loader import get_template
 from allauth.account.decorators import verified_email_required
 
 # Create your views here.
@@ -20,6 +22,16 @@ class UserHomePage(TemplateView):
     def is_user_staff(self):
         return self.request.user.is_staff
 
+    def retrieve_profile_from_request(self):
+        """
+        Returns a queryset containing a users profile if one exists.
+        """
+        user = self.request.user
+        try:
+            return user.profile
+        except ObjectDoesNotExist:
+            return None
+
     def get(self, request, *args, **kwargs):
         """
         Handles GET request.
@@ -28,4 +40,20 @@ class UserHomePage(TemplateView):
             pass
         else:
             self.extra_context.update({'username': self.request.user.username})
+            user_profile = self.retrieve_profile_from_request()
+            user_profile_data = user_profile.retrieve_field_data() if user_profile else {}
+            if user_profile:
+                first_login = False
+                user_profile_data.pop('username')
+                user_address = user_profile_data.pop('address')
+                user_address_data = user_address.retrieve_field_data()
+                for field in ['username', 'latitude', 'longitude']:
+                    user_address_data.pop(field)
+                profile_template = get_template('home/profile.html')
+                rendered_profile_template = profile_template.render({'user_profile_data': user_profile_data.items(),
+                                                                     'user_address_data': user_address_data.items()})
+                self.extra_context.update({'profile': rendered_profile_template})
+            else:
+                first_login = True
+                pass
         return super().get(request, *args, **kwargs)
