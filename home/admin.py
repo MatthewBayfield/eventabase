ccd05b616a2ad9e88c6c6fd0b7bfabef780df75d
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-10-27 19:14:06 +0000

    Register the home app models in the admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import UserAddress, UserProfile
 
 # Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    """
+    pass
+
+@admin.register(UserAddress)
+class UserAddressAdmin(admin.ModelAdmin):
+    """
+    """
+    pass
