a9197b395a4f6e8df9f86a4e6c44b836dafb9f40
MatthewBayfield <matthew.bayfield@yahoo.com>
2023-06-11 21:12:49 +0000

    Update events_and_activities urls with path for RetrieveContactInfoView

--- events_and_activities/urls.py
+++ events_and_activities/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from events_and_activities.views import SearchAdvertsView
+from events_and_activities.views import SearchAdvertsView, RetrieveContactInfoView
 
 app_name = 'events_and_activities'
 urlpatterns = [
     path('search_event_adverts/', SearchAdvertsView.as_view(extra_context={'page_id': 'search_adverts',
                                                                            'logged_in': True}),
-         name='search_adverts_page')
+         name='search_adverts_page'),
+    path('retrieve_contact_info/', RetrieveContactInfoView.as_view(), name='retrieve_contact_info')
 ]
